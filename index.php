05480c4069003b3e393fcc95b0b095f5fc1f7c71
unknown <jdanielson735@wit-xdvm-027.MILLARDPS.NET>
2012-12-03 11:57:23 -0600

    End of day 1

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
 <?php
+
+// Load DB constats
+require('config/db.php');
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="icon" type="image/png" href="img/blog-icon.png" />
+		
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="styles.css" />
+		
+		<title>Jake's Blog</title>
+	</head>
+	<body>
+		<div id="wrapper">
+			<header>
+				<?php include('layout/header.php')?>
+			</header>
+			<nav>
+				<?php include('layout/nav.php')?>
+			</nav>
+			<div id="content">
+				<?php include('layout/content.php')?>
+			</div>
+			<footer>
+				<?php include('layout/footer.php')?>
+			</footer>
+		</div>
+	</body>
+</html>
--- layout/nav.php
+++ layout/nav.php
@@ -1,1 +1,11 @@
-<?php
+<div class="navbar">
+	<div class="navbar-inner">
+		<div class="container">
+			<a href="./" class="brand"><i class="icon-book"></i></a>
+			<ul class="nav">
+				<li><a href="./?p=post">Posts</a></li>
+				<li><a href="./?p=form_add_post">Add</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
